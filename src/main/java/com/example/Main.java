91cb667cfc6245aa81a46a441f25a0b55e6bc290
lai2-24 <lai2-24@itismeucci.com>
2025-09-30 13:35:58 +0200

    created socket example

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,24 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server socket in ascolto sulla porta 3000");
+        ServerSocket ss = new ServerSocket(3000);
+        Socket mioSocket = ss.accept();
+        BufferedReader in = new BufferedReader(new InputStreamReader(mioSocket.getInputStream()));
+        PrintWriter out = new PrintWriter(mioSocket.getOutputStream());
+
+        out.println("ciao");
+        String s = in.readLine();
+        System.out.println("la stringa ricevuta e' " + s);
+        System.out.println("Qualcuno si e' collegato");
+        ss.close();
     }
 }
